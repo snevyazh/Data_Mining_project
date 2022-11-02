98b62854b5c8729d6304111915782cec9b63b65b
Stanislav Nevyazhskiy <snevyazh@gmail.com>
2022-11-02 14:52:30 +0200

    adding html file

--- parser.py
+++ parser.py
@@ -1,11 +1,15 @@
 import requests
+from bs4 import BeautifulSoup
 
 URL = 'https://www.glassdoor.com/Search/results.htm'
 request = '?keyword='
-search_string = 'engineer' # input('enter search word ')
+search_string = 'engineer'  # input('enter search word ')
 response = requests.get(URL + request + search_string)
+
 print(response)
-
-
 print(response.headers)
-print(response.text)
+# print(response.text)
+with open('/Users/stanislavnevyazhsky/My Drive/Data Science/Python/data mining/output.html', 'w') as output:
+    output.write(response.text)
+
+
