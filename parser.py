ad929ec69e5054f6bddb88db7540f1f6943d650d
Stanislav Nevyazhskiy <snevyazh@gmail.com>
2022-11-05 09:07:22 +0200

    playing with news page

--- parser.py
+++ parser.py
@@ -1,11 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://finance.yahoo.com/quote/'
+URL = 'https://finance.yahoo.com/news/'
 request1 = '?p='
 request2 = '&.tsrc=fin-srch'
 search_string = 'AMD'  # input('enter search ticker ')
-response = requests.get(URL + search_string + request1 + search_string + request2)
+response = requests.get(URL) #  + search_string + request1 + search_string + request2
 soup = BeautifulSoup(response.content, "html.parser")
 print(response)
 print(response.headers)
@@ -13,4 +13,7 @@ print(response.headers)
 with open('/Users/stanislavnevyazhsky/My Drive/Data Science/Python/data mining/output1.html', 'w') as output:
     output.write(soup.prettify())
 
+author = soup.find_all('author')
+print(author) # no reference for an article on the page
+##need probably to look for links to every article and get info there
 
