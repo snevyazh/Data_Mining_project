b4b5d933cddd26b326268e5c3f1ebe70aa2ad8d9
Stanislav Nevyazhskiy <snevyazh@gmail.com>
2022-11-06 18:34:27 +0200

    lister added

---
+++ lister.py
@@ -0,0 +1,42 @@
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+url_root_yahoo = 'https://finance.yahoo.com/news/'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36'}
+
+browser = driver = webdriver.Chrome()
+browser.get(url_root_yahoo)
+
+
+def scroll_to_bottom(driver):
+    old_position = 0
+    new_position = None
+    while new_position != old_position:
+        # Get old scroll position
+        old_position = driver.execute_script(
+                ("return (window.pageYOffset !== undefined) ?"
+                 " window.pageYOffset : (document.documentElement ||"
+                 " document.body.parentNode || document.body);"))
+        # Sleep and Scroll
+        time.sleep(1)
+        driver.execute_script((
+                "var scrollingElement = (document.scrollingElement ||"
+                " document.body);scrollingElement.scrollTop ="
+                " scrollingElement.scrollHeight;"))
+        # Get new position
+        new_position = driver.execute_script(
+                ("return (window.pageYOffset !== undefined) ?"
+                 " window.pageYOffset : (document.documentElement ||"
+                 " document.body.parentNode || document.body);"))
+
+
+scroll_to_bottom(driver)
+text = driver.page_source
+with open('/Users/stanislavnevyazhsky/My Drive/Data Science/Python/data mining/output11111.html', 'w') as output:
+    output.write(text)
+
+
+browser.close()
