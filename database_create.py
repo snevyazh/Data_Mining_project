c623b9483c63d0e85087e86a278e260b47bc8a20
Stanislav Nevyazhskiy <snevyazh@gmail.com>
2022-11-17 15:35:26 +0200

    added database creation script database_create.py

---
+++ database_create.py
@@ -0,0 +1,62 @@
+from datetime import datetime
+import pymysql
+
+USER = 'root'
+PASSWORD = 'barmaglot'
+
+connection = pymysql.connect(host='localhost',
+                             user=USER,
+                             password=PASSWORD,
+                             )
+
+
+def run_sql(sql_command):
+    """function receives the SQL query and runs it """
+    with connection.cursor() as cursor:
+        cursor.execute(sql_command)
+        result = cursor.fetchall()
+        connection.commit()
+    return result
+
+
+def create_database():
+    """creates the database with desired tables to store news"""
+
+    run_sql("""create database if not exists yahoo;""")
+    run_sql("""use yahoo;""")
+    run_sql("""CREATE TABLE IF NOT EXISTS news 
+        (
+        ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(45) NULL,
+        author VARCHAR(45) NULL,
+        news_date DATETIME NULL,
+        news_text VARCHAR(9999) NULL,
+        url VARCHAR(45) NULL
+        )
+        ;""")
+    run_sql("""CREATE TABLE IF NOT EXISTS yahoo.ticker (
+          ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+          ticker_name VARCHAR(45) NULL)
+            ;""")
+
+    run_sql("""CREATE TABLE IF NOT EXISTS yahoo.news_ticker (
+        ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        ticker_id int ,
+        news_id int, 
+        
+        index (ticker_id),
+        index(news_id),
+        
+        FOREIGN KEY (ticker_id)
+            REFERENCES ticker(ID)
+            ON UPDATE CASCADE ON DELETE RESTRICT,
+        FOREIGN KEY (news_id)
+            REFERENCES news(ID)
+            ON UPDATE CASCADE ON DELETE RESTRICT
+        )
+        ;""")
+
+
+create_database()
+
+
