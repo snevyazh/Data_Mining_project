89259d9de5a212afb6ddd0ea4fecc4d82fc07071
Kuzma Tsukanov <kuzmatsukanov@gmail.com>
2022-12-04 18:27:02 +0200

    api_data_ver_kuzma.py rewritten to class ExtractorApi()

--- api_data_ver_kuzma.py
+++ api_data_ver_kuzma.py
@@ -1,9 +1,11 @@
 import requests
 import json
 import pandas as pd
+import datetime
 import matplotlib.pyplot as plt
 
-class api_data():
+
+class ExtractorApi():
     def __init__(self):
         return
 
@@ -22,15 +24,46 @@ class api_data():
         #     print(response.status_code)
         return response
 
-# Price history
-url = "https://query1.finance.yahoo.com/v8/finance/chart/bmw.de"
+    def get_price_data(self, ticker = "bmw.de", interval="1d", range="30d",
+                       period1=datetime.datetime(2022, 1, 1),
+                       period2=datetime.datetime(2022, 1, 7)):
+        """
+        Gets price data
+        :param ticker: (str) e.g. "bmw.de"
+        :param interval: (str) # 1m 2m 5m 15m 30m 60m 90m 1h 1d 5d 1wk 1mo 3mo
+        :param range: (str) e.g. "30d". Can be "max" or "previous"
+        :param period1: (datetime.datetime) time to start at (can be overwritten by 'range')
+        :param period2: (datetime.datetime) time to end at
+        :return: DataFrame of prices over time
+        """
 
-api_obj = api_data()
-response = api_obj.url_retrieve(url)
-json_content = response.content
-dict_content = json.loads(json_content)
+        period1 = str(int(period1.timestamp()))  # UNIX timestamp representation of the date you wish to start at
+        period2 = str(int(period2.timestamp()))
 
-time_lst = dict_content['chart']['result'][0]['timestamp']
-time_lst = pd.to_datetime(time_lst, unit='s')
-data_lst = dict_content['chart']['result'][0]['indicators']['quote'][0]
-df_table = pd.DataFrame(data=data_lst, index=time_lst)
+        url = "https://query1.finance.yahoo.com/v8/finance/chart/{}?&interval={}&range={}&period1={}&period2={}" \
+            .format(ticker, interval, range, period1, period2)
+
+        # url = "https://query1.finance.yahoo.com/v8/finance/chart/{}?&interval={}&range={}" \
+        #     .format(ticker, interval, range)
+
+        response = self.url_retrieve(url)
+        json_content = response.content
+        dict_content = json.loads(json_content)
+
+        time_lst = dict_content['chart']['result'][0]['timestamp']
+        time_lst = pd.to_datetime(time_lst, unit='s')
+        data_lst = dict_content['chart']['result'][0]['indicators']['quote'][0]
+        df_table = pd.DataFrame(data=data_lst, index=time_lst)
+        return df_table
+
+
+ticker = "bmw.de"
+interval = "1d"
+range = ""
+
+api_obj = ExtractorApi()
+df_table = api_obj.get_price_data(ticker=ticker,
+                                  interval=interval,
+                                  range=range,
+                                  period1=datetime.datetime(2022, 1, 1),
+                                  period2=datetime.datetime(2022, 1, 7))
