cc42bc332388a0aa542a7d4828819fb99b94e610
snevyazh <snevyazh@gmail.com>
2022-12-07 13:40:32 +0200

    edited read_cli_arguments.py added date_from and date_to that defines the range of dates to query. Added -u parameter that defines if to query API too or just news. Removed parameter number_of_news, the news will be scraped according to dates.

--- read_cli_argiuments.py
+++ read_cli_argiuments.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 
 
 def get_command_line_params():
@@ -6,23 +7,29 @@ def get_command_line_params():
     username , password, number of news to show and ticker symbol.
     Raises ValueError exception if number of news is not a number
     :param: none
-    :return: list of input parameters in form of a list
+    :return: list of input parameters in form of a list: username, password to DB, ticker name, dates FROM which to
+    parse data and UP TIL which date
         """
     parser = argparse.ArgumentParser()
-    parser.add_argument('-u', action='store_true', help="if selected URL won't be printed")
+    parser.add_argument('-u', action='store_true', help="if selected API won't be queried")
     parser.add_argument('username', type=str, help='username')
     parser.add_argument('password', type=str, help='password')
-    parser.add_argument('number_of_news', type=int, help='how many news cards to export')
+    # parser.add_argument('number_of_news', type=int, help='how many news cards to export')
     parser.add_argument('ticker', type=str, help='what ticker you need')
+    parser.add_argument('date_from', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+                        help='Date from which to parse, format year-month-day')
+    parser.add_argument('date_to', type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
+                        help='Up to which date to parse, format year-month-day')
     args = parser.parse_args()
-    if not str(args.number_of_news).isalpha:
-        raise ValueError("The number of news articles to show entered in command line is not a number")
+    # if not str(args.number_of_news).isalpha:
+    #     raise ValueError("The number of news articles to show entered in command line is not a number")
     input_parameters = {
-        "url_output": not args.u,
+        "api": not args.u,
         "username": args.username,
         "password": args.password,
-        "number_of_news": args.number_of_news,
-        "ticker": args.ticker
+        "ticker": args.ticker,
+        "date_from": args.date_from,
+        "date_to": args.date_to,
     }
     return input_parameters
 
