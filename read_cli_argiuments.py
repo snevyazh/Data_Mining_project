8b89b5d2c01cf1af7a7cedf5ef14fea91453ac52
snevyazh <snevyazh@gmail.com>
2022-12-07 18:18:43 +0200

    added logging to read_cli_argiuments.py . added checks for inputs

--- read_cli_argiuments.py
+++ read_cli_argiuments.py
@@ -1,5 +1,7 @@
 import argparse
 import datetime
+from logger import logger
+import sys
 
 
 def get_command_line_params():
@@ -21,18 +23,24 @@ def get_command_line_params():
                         help='Date from which to parse, format year-month-day')
     parser.add_argument('date_to', nargs=1, type=lambda s: datetime.datetime.strptime(s, '%Y-%m-%d'),
                         help='Up to which date to parse, format year-month-day')
-
     args = parser.parse_args()
-
+    logger.debug("User input {}".format(args))
+    if not isinstance(args.date_from[0], datetime.datetime):
+        logger.error("The start date {} is not a datetime.datetime type".format(args.date_from[0]))
+        sys.exit()
+    if not isinstance(args.date_to[0], datetime.datetime):
+        logger.error("The start date is not a datetime.datetime type")
+        sys.exit()
     if not str(args.number_of_news).isalpha:
-        raise ValueError("The number of news articles to show entered in command line is not a number")
-    input_parameters = {
-        "api": args.a,
-        "username": args.username,
-        "password": args.password,
-        "number_of_news": args.number_of_news,
-        "ticker": args.ticker,
-        "date_from": args.date_from[0],
-        "date_to": args.date_to[0],
-    }
+        logger.error("The number of news articles to show entered in command line is not a number")
+        sys.exit()
+    if (args.date_to[0] - args.date_from[0]).total_seconds() < 0:
+        logger.error(("The start date is later than the end day: {} and {}".format(args.date_from[0], args.date_to[0])))
+        sys.exit()
+    input_parameters = {"api": args.a, "username": args.username, "password": args.password,
+                        "number_of_news": args.number_of_news, "ticker": args.ticker, "date_from": args.date_from[0],
+                        "date_to": args.date_to[0]}
+    logger.debug("User input is parsed {}".format(input_parameters))
     return input_parameters
+
+print(get_command_line_params())
