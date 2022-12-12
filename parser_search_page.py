429dddf645728a5fe02ecb4799e7413e10316f99
snevyazh <snevyazh@gmail.com>
2022-12-12 14:51:30 +0000

    added webdriver corrections for headless

--- parser_search_page.py
+++ parser_search_page.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
 from logger import logger
+from selenium.webdriver.chrome.options import Options
 
 
 class ParserSearchPage:
@@ -12,7 +13,10 @@ class ParserSearchPage:
         :param ticker: (str) ticker of company, e.g. "BMW.DE"
         """
         self.ticker = ticker
-        self._driver = webdriver.Chrome()
+        options = Options()
+        options.add_argument('--headless')
+        options.add_argument("--no-sandbox")
+        self._driver = webdriver.Chrome(executable_path='/usr/bin/chromedriver', options=options)
         self._html_page = self._get_html_page()
         return
 
